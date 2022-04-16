ec427eecdb1c196536ed9663b58016b463be65a3
Manuel Ozan <manuozan97@gmail.com>
2022-04-16 19:30:19 -0300

    duración de video añadida
    en esta versión el juego tiene una especie de selectividad donde los contenedores del rompecabezas  no aceptan una imagen que no vaya en esa posición.

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ let PauseButton = document.getElementById("pause")
 
 let playing = false;
 
+
 PlayButton.addEventListener("click", function(){
  
     if (!playing) {
  
     video.play();
     playing = true;
-    console.log(playing)
+    console.log(playing);
+
+    // setInterval(() => {console.log(Math.round(video.currentTime)+'s')
+    //     },1000);
+    
 }});
 
 PauseButton.addEventListener("click", function(){
@@ -21,5 +26,34 @@ PauseButton.addEventListener("click", function(){
 
     video.pause();
     playing = false;
-    console.log(playing)
+    console.log(playing);  
 }});
+
+////////////////////// DURACIÓN DEL VIDEO //////////////////////////////
+
+// setInterval(()=>{document.getElementById('video-time').innerHTML=Math.round(video.currentTime)+'/'+Math.round(video.duration)
+// },1000);
+
+// video.addEventListener("timeupdate", (e) => {
+//     let currentVideoTime = e.target.currentTime;
+//     let currentMin = Math.floor(currentVideoTime / 60);
+//     let currentSec = Math.floor(currentVideoTime % 60);
+   
+//     currentSec < 10 ? (currentSec = "0" + currentSec) : currentSec;
+//     document.getElementById('video-time').innerHTML= `${currentMin} : ${currentSec}`;}
+// no sé porque no me anduvo
+
+video.addEventListener("timeupdate", function(){
+ 
+    let currentVideoTime = video.currentTime;
+    let currentMin = Math.floor(currentVideoTime / 60);
+    let currentSec = Math.floor(currentVideoTime % 60);
+     
+    const videoDuration = video.duration;
+    const durationMin = Math.floor(videoDuration / 60);
+    const durationSec = Math.floor(videoDuration % 60);
+
+    currentSec < 10 ? (currentSec = "0" + currentSec) : currentSec;
+    
+    document.getElementById('video-time').innerHTML= `${currentMin}:${currentSec} / ${durationMin}:${durationSec}`;
+});
